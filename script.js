6199407a228589b2e622b6d3aa3fb945bf060461
Ashton Lima <ashton@4rden.com>
2025-09-01 15:18:30 -0700

    added generative arrays for characters, symbols, and numbers. Determine random number using modulo and cryptographic randomness

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-const letters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-const symbols = [ "~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?", "/"];
-const numbers = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9",];
 
 // Toggle design function for button
 
@@ -35,25 +32,38 @@ let numberInput = document.getElementById("character-count")
   });
 
 
+// random numbers 
+
+function randomNumbers(minValue,maxValue){ 
+    let array = new Uint32Array(1)
+    window.crypto.getRandomValues(array)
+    return minValue + (array[0] % (maxValue - minValue))
+}
+
+
 // Generating the Password 
 const generatePassword = document.getElementById('btn-generate')
 const passwordText = document.getElementById('password-text')
 
 generatePassword.addEventListener('click', () => { 
     const value = parseInt(numberInput.value, 10);
-    let passwordArray = letters;
+
+    let passwordArray = Array.from({length: 26},(_,i) => 
+    String.fromCharCode(65 + i));
     
     if (document.getElementById('toggle1').classList.contains('on')) { 
-        passwordArray = passwordArray.concat(symbols);
+        passwordArray = passwordArray.concat(Array.from({length: 47-33 + 1},(_,i) =>
+        String.fromCharCode(33+i)));
     }
 
     if (document.getElementById('toggle2').classList.contains('on')) { 
-        passwordArray = passwordArray.concat(numbers)
+        passwordArray = passwordArray.concat(Array.from({length: 57 - 48 + 1},(_,i) => 
+        String.fromCharCode(48 + i) ))
     }
     
     let out = ""
     for (let i = 0 ; i < value ; i ++) { 
-        const randomNumber = Math.floor(Math.random() * passwordArray.length)
+        let randomNumber = randomNumbers(0,passwordArray.length)
         out += passwordArray[randomNumber]
     }
 
